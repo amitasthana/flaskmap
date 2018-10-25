4a551ac5487bdc45756c089c1642804cc77eb2b7
Amit Asthana <amitasthana.777@gmail.com>
2018-10-25 11:37:52 +0530

    maps app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'OK!'
+	return render_template('map.html')
 
 if __name__ == '__main__':
 	app.run()
